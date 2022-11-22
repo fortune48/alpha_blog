10caa0205dfdf6663ce4b3504ca10d5fd5b408b4
fortune48 <ukwuomafortune01@gmail.com>
2022-11-22 01:58:48 +0100

    add user index page and style it

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
 
+  def index
+    @users = User.all
+  end
+
   def show
     @user = User.find(params[:id])
     @articles = @user.articles
@@ -27,7 +31,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Your account information wassuccessful updated."
-      redirect_to articles_path
+      redirect_to @user
     else
       render "edit"
     end
