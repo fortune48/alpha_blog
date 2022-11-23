1532d46124e19a9804b3173c6038bbd7be065147
fortune48 <ukwuomafortune01@gmail.com>
2022-11-23 11:48:08 +0100

    edited validations in the articles database model

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,7 @@ class Article < ApplicationRecord
 
   belongs_to :user
 
-  validates :title, presence: true, length: { minimum: 3, maximum: 30 }
-  validates :description, presence: true, length: { minimum: 10, maximum: 300 }
+  validates :title, presence: true, length: { minimum: 3, maximum: 100 }
+  validates :description, presence: true, length: { minimum: 10, maximum: 1000 }
 
 end
